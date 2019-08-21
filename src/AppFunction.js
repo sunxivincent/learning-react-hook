a0b3e89ae3893b6da4eb86c612a438daa049ce53
xi <xisun1990@gmail.com>
2019-08-20 21:45:44 -0700

    geo location API

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null,
+};
+
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
   const [status, setStatus] = useState(navigator.onLine);
+  const [location, setLocation] = useState(initialLocationState);
+  let mounted = true;
 
   useEffect(() => {
     document.title = `you have clicked ${count} times`;
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('online', handleOnline);
     window.addEventListener('offline', handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeoLocation);
+
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('online', handleOnline);
       window.removeEventListener('offline', handleOffline);
+      mounted = false;
     }
   }, [count]);
 
@@ -41,6 +52,16 @@ const App = () => {
     setStatus(false)
   };
 
+  const handleGeoLocation = (event) => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed,
+      });
+    }
+  };
+
   return (
     <>
       <h2>Counter</h2>
@@ -61,6 +82,11 @@ const App = () => {
 
       <h2>Network status</h2>
       <p>You are currently <strong>{status ? "online" : "offline"}</strong></p>
+
+      <h2>Geo Location</h2>
+      <p>Latitude: {location.latitude}</p>
+      <p>Longitude: {location.longitude}</p>
+      <p>Speed: {location.speed ? location.speed:0}</p>
     </>
   );
 };
